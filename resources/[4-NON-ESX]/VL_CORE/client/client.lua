01ea56a9a2a9e5a5a2df6a5abc014bc273e8ecf9
RetrozDev <ryanbarbozapro@gmail.com>
2024-02-29 01:43:34 +0100

    Ajout concess + fix bug du bug de la bdd

--- resources/[4-NON-ESX]/VL_CORE/client/client.lua
+++ resources/[4-NON-ESX]/VL_CORE/client/client.lua
@@ -107,26 +107,26 @@ if Config.motorcyclehelmet == false then
 end
 
 -- Coords Commands
-ShowNotificationTicker = function(message)
-    BeginTextCommandThefeedPost('STRING')
-    AddTextComponentSubstringPlayerName(message)
-    EndTextCommandThefeedPostTicker(0, 1)
-end
+-- ShowNotificationTicker = function(message)
+--     BeginTextCommandThefeedPost('STRING')
+--     AddTextComponentSubstringPlayerName(message)
+--     EndTextCommandThefeedPostTicker(0, 1)
+-- end
 
-RegisterCommand('coords', function()
-    local coords, heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
-    SendNUIMessage({
-        type = 'clipboard',
-        data = '' .. vec(coords.x, coords.y, coords.z, heading)
-    })
-    ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y, coords.z, heading))
-end)
+-- RegisterCommand('coords', function()
+--     local coords, heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
+--     SendNUIMessage({
+--         type = 'clipboard',
+--         data = '' .. vec(coords.x, coords.y, coords.z, heading)
+--     })
+--     ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y, coords.z, heading))
+-- end)
 
-RegisterCommand('camcoords', function()
-    local coords, heading = GetFinalRenderedCamCoord(), GetFinalRenderedCamRot(0)
-    SendNUIMessage({
-        type = 'clipboard',
-        data = '{' .. vec(coords.x, coords.y, coords.z) .. ', ' .. vec(heading.x, heading.y, heading.z) .. ' }'
-    })
-    ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y, coords.z) .. ', ' .. vec(heading.x, heading.y, heading.z) )
-end)
+-- RegisterCommand('camcoords', function()
+--     local coords, heading = GetFinalRenderedCamCoord(), GetFinalRenderedCamRot(0)
+--     SendNUIMessage({
+--         type = 'clipboard',
+--         data = '{' .. vec(coords.x, coords.y, coords.z) .. ', ' .. vec(heading.x, heading.y, heading.z) .. ' }'
+--     })
+--     ShowNotificationTicker('Copied to clipboard! ' .. vec(coords.x, coords.y, coords.z) .. ', ' .. vec(heading.x, heading.y, heading.z) )
+-- end)
